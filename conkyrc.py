649fb8d012c3b3147a2cfec1a0bde37b038202f4
samyboy <samuel.krieg+github@gmail.com>
2014-07-21 03:03:12 +0200

    ajout d'une macro

--- conkyrc.py
+++ conkyrc.py
@@ -13,8 +13,23 @@ template = templateEnv.get_template( TEMPLATE_FILE )
 
 
 # Specify any input variables to the template as a dictionary.
-templateVars = { "title" : "Test Example",
-                 "description" : "A simple inquiry of function." }
+
+maximum_width = 320
+
+templateVars = { "maximum_width"      : str(maximum_width),
+                 "graph_width"        : str(maximum_width),
+                 "graph_width_small"  : str(int(maximum_width/2)),
+                 "graph_height"       : "24",
+                 "graph_height_small" : "24",
+                 "bar_width"          : str(maximum_width),
+                 "bar_width_small"    : "155",
+                 "bar_height"         : "12",
+                 "bar_height_half"    : "12",
+                 "color_term"         : "grey",
+                 "color_value"        : "FFFFFF",
+                 "color_graph1"       : "F700FF",
+                 "color_graph2"       : "FFFFFF",
+}
 
 # Finally, process the template to produce our final text.
 outputText = template.render( templateVars )
