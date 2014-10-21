4d55f1526e4a4b393e5ba62d44309cd479377565
Krieg, Samuel <samuel.krieg@sicpa.com>
2014-10-21 10:09:17 +0200

    install bepo file

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 /bin/cp ./shell_aliases ~/.shell_aliases
 /bin/cp ./bashrc ~/.bashrc
 /bin/cp ./vimrc ~/.vimrc
+/bin/cp ./vimrc.bepo ~/.vimrc.bepo
 /bin/cp ./gitconfig ~/.gitconfig
 /bin/cp ./gitignore-global ~/.gitignore-global
 ./conkyrc.py > ~/.conkyrc
