d0774fb5b9e4f75a2d648dff300c26f6f5550bd5
samyboy <samuel.krieg+github@gmail.com>
2016-10-17 21:01:37 +0200

    ajout de fichiers tempoeraires

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+/bin/cp ./zshrc ~/.zshrc
+/bin/cp ./shell_aliases ~/.shell_aliases
+/bin/cp ./bashrc ~/.bashrc
+/bin/cp ./vimrc ~/.vimrc
+/bin/cp ./gitconfig ~/.gitconfig
+/bin/cp ./gitignore-global ~/.gitignore-global
+./conkyrc.py > ~/.conkyrc
