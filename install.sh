53216bfcb6743b316a0724f376beeed96599e4cb
Samuel Krieg <samuel.krieg+github@gmail.com>
2019-08-28 17:25:15 +0200

    Document install script

--- install.sh
+++ install.sh
@@ -5,6 +5,18 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 CP="/bin/cp -av"
 
+# This is a nice title
+title() {
+    echo
+    echo " $*"
+    echo "-----------------"
+}
+
+# Clone a Git repository
+# arguments:
+# 1. The URL of the repo
+# 2. The target folder
+# 3. The Git ref (optional)
 gitget() {
     url=$1
     folder=$2
@@ -40,8 +52,11 @@ gitget() {
     cd $DIR
 }
 
+# Clone a Gist file (from Github") and put it as a command in the .local/bin directory
+# Arguments:
+# 1. The target command name
+# 2. The Gist ID
 function gist() {
-    set -x
     cmd=$1
     gist_id=$2
     echo Downloading Gist $gist_id as $cmd...
@@ -50,7 +65,7 @@ function gist() {
 
 }
 
-echo "Installing files..."
+title "Installing dot files..."
 
 $CP ./shell_aliases ~/.shell_aliases
 $CP ./bashrc ~/.bashrc
@@ -58,23 +73,24 @@ $CP ./gitconfig ~/.gitconfig
 $CP ./gitignore-global ~/.gitignore-global
 $CP ./tmux.conf ~/.tmux.conf
 $CP ./ackrc ~/.ackrc
-
+# For nice colors with the ls command ( https://github.com/seebi/dircolors-solarized )
+$CP dircolors.ansi-dark ~/.dircolors
 set +e # ignore jinja2 import errors
 ./conkyrc.py > ~/.conkyrc
 set -e
+
+title "Install files in .local/bin"
 
 mkdir -p ~/.local/bin
 $CP ./bin/* ~/.local/bin
 
 gist "git-fetch-all" "eef091d73879f8d0d5661efc834e69dc"
 
-# https://github.com/seebi/dircolors-solarized
-/bin/cp dircolors.ansi-dark ~/.dircolors
-
-# zsh
+title "Installing my ZSH workplace"
 [ ! -d ~/.zsh ] && mkdir -p ~/.zsh
 $CP ./zshrc ~/.zshrc
 
+# LiquidPrompt
 gitget https://github.com/SamK/liquidprompt.git ~/.zsh/liquidprompt fix/do-not-redeclare
 
 # A lot of completion features for ZSH
@@ -83,29 +99,25 @@ gitget https://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions
 curl https://raw.githubusercontent.com/borgbackup/borg/1.1.5/scripts/shell_completions/zsh/_borg \
 > ~/.zsh/zsh-completions/src/_borg
 
-# Vim
-
+title Vim
 $CP ./vimrc ~/.vimrc
 mkdir -p ~/.vim/
 
-
-
-# Vim solarized
+# TODO: tmpdir
 if [ -d solarized ]; then
-    echo "Updating solarized..."
+    echo "Updating solarized theme for Vim..."
     cd solarized
     git pull
     cd ..
 else
-    echo "Installing solarized..."
+    echo "Installing solarized theme for Vim..."
     git clone https://github.com/altercation/solarized.git
 fi
-/bin/cp -a ./solarized/vim-colors-solarized/colors ~/.vim/
+$CP ./solarized/vim-colors-solarized/colors ~/.vim/
 
-# VIM-Plug
+echo VIM-Plug...
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# Install plugins
+echo Installing Vim plugins...
 vim +PlugInstall +qall
 
 echo "Setup completed."
-
