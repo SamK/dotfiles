c7df364e4c75976610d58ef6b6c0d4cca1cea386
Samuel Krieg <samuel.krieg+github@gmail.com>
2023-02-10 08:42:35 +0100

    feat: tmpclean

--- install.sh
+++ install.sh
@@ -44,6 +44,7 @@ create_dotlink .local/bin/git-branch-deletable
 create_dotlink .local/bin/git-branches-authors
 create_dotlink .local/bin/git-find-md5
 create_dotlink .local/bin/git-ls-wip
+create_dotlink .local/bin/tmpclean
 
 title "Install files in .local/share"
 
