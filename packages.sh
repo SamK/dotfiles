2ebe8b374757bac6e9f9ceae4e70f758b6b1e77d
Samuel Krieg <samuel.krieg+github@gmail.com>
2023-03-16 15:10:28 +0100

    feat: packages.sh

---
+++ packages.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+FAVOURITES="
+bind9-dnsutils
+diffutils
+dos2unix
+python3-venv
+ack
+lynx
+jq
+gzip
+zsh
+shellcheck
+ncdu
+git
+"
+
+package_present () {
+    dpkg -s $1 2>/dev/null > /dev/null
+}
+
+
+echo Missing favourite packages:
+for package in $FAVOURITES; do
+    if  ! package_present $package; then
+        echo $package
+    fi
+done
